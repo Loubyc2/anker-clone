f2e11f2eb980ef7c3e626db72bd0dbb85fb5fa91
Louby <loubyc2@gmail.com>
2021-03-29 01:39:47 -0300

    addd title to iframe

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,7 +19,9 @@ function Footer() {
                 </div>
                 <div className="footer__direction">
                     <h2>Kote w ap jwenn nou</h2>
-                    <iframe className='footer__iframe' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3326.31745694575!2d-70.69125428516436!3d-33.51913098075431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662db15b0d5d20b%3A0x6e48e639176ddb4b!2sSta%20Anita%207555%2C%20Lo%20Espejo%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1sen!2scl!4v1616991203291!5m2!1sen!2scl" />
+                    <iframe className='footer__iframe' title="google direction"
+                        src="https://www.google.com/maps/
+                        embed?pb=!1m18!1m12!1m3!1d3326.31745694575!2d-70.69125428516436!3d-33.51913098075431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662db15b0d5d20b%3A0x6e48e639176ddb4b!2sSta%20Anita%207555%2C%20Lo%20Espejo%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1sen!2scl!4v1616991203291!5m2!1sen!2scl" />
                 </div>
                 <div className="footer__maps"></div>
             </div>
